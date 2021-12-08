532d6ce935be03602ae5b6c98fead73848f11807
k-aoki0699 <k-aoki@ndkits.co.jp>
2021-12-09 08:51:47 +0900

    3sln added

---
+++ folder02/Project2/src/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2.sorce
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld 02!!");
+        }
+    }
+}
